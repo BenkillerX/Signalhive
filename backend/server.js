53a2bad6d0d6857fa70e29d58f3fab227849bfa8
BenKillerX <benedictakhere802@gmail.com>
2026-09-22 11:49:31 +0100

    Debugging currently

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,12 @@ const app = express()
 const PORT = process.env.PORT;
 app.use(express.json())
 app.use(cors())
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
+app.get("/", (req, res) => {
+  res.json({
+    message: "SignalHive API is running",
+  });
+});
 connectDB().then(()=>{
     app.listen(PORT,()=>{
         console.log(`Server running on ${PORT}`);
